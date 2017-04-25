5fabf230fd61421b6f8870b22fbb20815aab577f
libai <hebilaolitou@gmail.com>
2017-04-25 13:57:14 +0800

    提交

---
+++ AppTest/src/main/java/com/test/IDexTestImpl.java
@@ -0,0 +1,12 @@
+package com.test;
+
+/**
+ * Created by guochenli on 2017/4/21.
+ */
+
+public class IDexTestImpl implements IDexTest {
+    @Override
+    public String getText() {
+        return "我是SD卡上的APK";
+    }
+}
---
+++ dexapplib/src/main/java/com/yibao/test/IDexTest.java
@@ -0,0 +1,10 @@
+package com.yibao.test;
+
+/**
+ * Created by guochenli on 2017/4/21.
+ */
+
+public interface IDexTest {
+
+    String  getText();
+}
