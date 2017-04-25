31bcbe1076bd6498a855cd2656aafd86b00f2167
libai <hebilaolitou@gmail.com>
2017-04-25 18:34:35 +0800

    更新，增加加载资源

--- dexclassloaderapp/src/main/java/com/dexclassloader/demo/MainActivity.java
+++ dexclassloaderapp/src/main/java/com/dexclassloader/demo/MainActivity.java
@@ -1,6 +1,12 @@
 package com.dexclassloader.demo;
 
 import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.res.AssetManager;
+import android.content.res.Resources;
 import android.os.Environment;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
@@ -13,6 +19,7 @@ import android.widget.Toast;
 
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Enumeration;
@@ -26,19 +33,21 @@ public class MainActivity extends AppCompatActivity {
     private static Handler mHandler = new Handler();
     private ProgressDialog pd;
 
+    public static final String LOCALDIR=Environment.getExternalStorageDirectory().getPath() + File.separator + "apptest-release.apk";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        pd=new ProgressDialog(this);
+        pd = new ProgressDialog(this);
         pd.setTitle("正在加载SD卡上的类");
         pd.setMessage("请稍后....");
 
         findViewById(R.id.test).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(pd!=null&&!pd.isShowing()){
+                if (pd != null && !pd.isShowing()) {
                     pd.show();
                 }
                 new Thread() {
@@ -46,6 +55,22 @@ public class MainActivity extends AppCompatActivity {
                     public void run() {
                         super.run();
                         load();
+                    }
+                }.start();
+            }
+        });
+
+        findViewById(R.id.test_res).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (pd != null && !pd.isShowing()) {
+                    pd.show();
+                }
+                new Thread() {
+                    @Override
+                    public void run() {
+                        super.run();
+                        loadRes();
                     }
                 }.start();
             }
@@ -62,7 +87,92 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    /**
+     * 加载资源
+     */
+    private void loadRes() {
+        String packagename = getUninstallApkInfo(this, LOCALDIR);
+        try {
+            dynamicLoadApk(LOCALDIR, packagename);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
+    /**
+     * 获取未安装apk的信息
+     *
+     * @param context
+     * @param archiveFilePath apk文件的path
+     * @return
+     */
+    private String getUninstallApkInfo(Context context, String archiveFilePath) {
+        String pkgName=null;
+        PackageManager pm = context.getPackageManager();
+        PackageInfo pkgInfo = pm.getPackageArchiveInfo(archiveFilePath, PackageManager.GET_ACTIVITIES);
+        if (pkgInfo != null) {
+            ApplicationInfo appInfo = pkgInfo.applicationInfo;
+//            String versionName = pkgInfo.versionName;//版本号
+//            Drawable icon = pm.getApplicationIcon(appInfo);//图标
+            String appName = pm.getApplicationLabel(appInfo).toString();//app名称
+            pkgName = appInfo.packageName;//包名
+//            info[0] = appName;
+        }
+        return pkgName;
+    }
+
+    /**
+     * @param DexPath
+     * @return 得到对应插件的Resource对象
+     */
+    private Resources getPluginResources(String DexPath) {
+        try {
+            AssetManager assetManager = AssetManager.class.newInstance();
+            Method addAssetPath = assetManager.getClass().getMethod("addAssetPath", String.class);//反射调用方法addAssetPath(String path)
+            //第二个参数是apk的路径：Environment.getExternalStorageDirectory().getPath()+File.separator+"plugin"+File.separator+"apkplugin.apk"
+            addAssetPath.invoke(assetManager, DexPath);//将未安装的Apk文件的添加进AssetManager中，第二个参数为apk文件的路径带apk名
+            Resources superRes = this.getResources();
+            Resources mResources = new Resources(assetManager, superRes.getDisplayMetrics(),
+                    superRes.getConfiguration());
+            return mResources;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+    /**
+     * 加载apk获得内部资源
+     *
+     * @param apkDir  apk目录
+     * @throws Exception
+     */
+    private void dynamicLoadApk(String apkDir, String apkPackageName) throws Exception {
+
+        DexClassLoader dexClassLoader =
+                new DexClassLoader(apkDir, getExternalCacheDir().getAbsolutePath(), null, getClassLoader());
+
+        Class<?> clazz = dexClassLoader.loadClass(apkPackageName + ".R$string");//通过使用apk自己的类加载器，反射出R类中相应的内部类进而获取我们需要的资源id
+        Field field = clazz.getDeclaredField("showtext");//得到名为one的这张图片字段
+        final int resId = field.getInt(R.id.class);//得到图片id
+        final Resources mResources = getPluginResources(apkDir);//得到插件apk中的Resource
+        if (mResources != null) {
+            //通过插件apk中的Resource得到resId对应的资源
+            mHandler.post(new Runnable() {
+                @Override
+                public void run() {
+                    pd.dismiss();
+                    ((TextView) findViewById(R.id.main_res)).setText(mResources.getString(resId));
+                }
+            });
+
+        }
+    }
+
+    /**
+     * 加载类
+     */
     private void load() {
         // 获取到包含 class.dex 的 jar 包文件
         final File apkFile =
@@ -75,14 +185,14 @@ public class MainActivity extends AppCompatActivity {
                 @Override
                 public void run() {
                     pd.dismiss();
-                    Toast.makeText(MainActivity.this,"文件不存在",Toast.LENGTH_LONG);
+                    Toast.makeText(MainActivity.this, "文件不存在", Toast.LENGTH_LONG);
 
                 }
             });
             return;
         }
 
-        if(!apkFile.canRead()){
+        if (!apkFile.canRead()) {
             // 如果没有读权限,确定你在 AndroidManifest 中是否声明了读写权限
             // 如果是6.0以上手机要查看手机的权限管理，你的这个app是否具有读写权限
             Log.d("LGC", "apkFile.canRead()= " + apkFile.canRead());
@@ -90,7 +200,7 @@ public class MainActivity extends AppCompatActivity {
                 @Override
                 public void run() {
                     pd.dismiss();
-                    Toast.makeText(MainActivity.this,"没有读写权限",Toast.LENGTH_LONG);
+                    Toast.makeText(MainActivity.this, "没有读写权限", Toast.LENGTH_LONG);
 
                 }
             });
